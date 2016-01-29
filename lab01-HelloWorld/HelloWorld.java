97f6dceefd6c5d891a19b45f720633deae9b082c
willg219 <willgilbert427@gmail.com>
2016-01-29 15:39:51 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////
+//Will Gilbert
+//lab 1
+//Hello World Java Program
+//January 29, 2016
+//
+//  first compile the program
+//      javac HelloWorld.java 
+//  run the program
+//      java HelloWorld//
+//  define a class
+public class HelloWorld{
+// add main method
+public static void main(String[] args){
+    
+System.out.println("###########################");
+System.out.println("###########################");
+System.out.println("#                         #");
+System.out.println("#    Hello World 2016!!   #");
+System.out.println("#                         #");
+System.out.println("###########################");
+System.out.println("###########################");
+System.out.println("test");
+//test/testtest
+}
+
+
+}
