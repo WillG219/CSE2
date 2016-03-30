72c968bcc49f508df81155be25a37bce02b6e0c3
willg219 <willgilbert427@gmail.com>
2016-03-30 19:32:54 +0000

    updated Rectangularize.java

--- hw09/Rectangularize.java
+++ hw09/Rectangularize.java
@@ -3,3 +3,33 @@
 //CSE 2
 //HW 9
 //Program 2
+
+import java.util.Scanner;
+public class Rectangularize{
+    public static void recMeth(int x){
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < x; j++){
+                System.out.print("%");
+            }
+            System.out.println();
+        }
+    }
+    public static void recMeth(String y){
+        for(int i = 0; i < y.length(); i++){
+            System.out.println(y);
+        }
+    }
+    public static void main(String[] args){
+        Scanner rectan = new Scanner(System.in);
+        System.out.println("Please enter an input: ");
+        if(rectan.hasNextInt()){
+            int x = rectan.nextInt();
+            recMeth(x);
+        }
+        else {
+            String x = rectan.nextLine();
+            recMeth(x);
+        }
+    }
+}
+    
